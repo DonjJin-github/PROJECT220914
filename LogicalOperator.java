fe15e1cb93931b2b8715bd15db653bd0f4eb46b2
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2022-09-20 20:03:28 +0900

    Update LogicalOperator.java

--- LogicalOperator.java
+++ LogicalOperator.java
@@ -1,6 +1,6 @@
 public class LogicalOperator {
     public static void main(String[] args) {
-        // 비교 연산
+       
         System.out.println('a' > 'b');      // false
         System.out.println(3 >= 2);         // true
         System.out.println(-1 < 0);         // true
@@ -9,7 +9,7 @@ public class LogicalOperator {
         System.out.println(3 != 2);         // true
         System.out.println(!(3 != 2));      // false
 
-        // 비교 연산과 논리 연산 복합
+        
         System.out.println((3 > 2) && (3 > 4));     // true && false    = false
         System.out.println((3 != 2) || (-1 > 0));   // true ||          = true
         System.out.println((3 != 2) ^ (-1> 0));     // true ^ false     = true
