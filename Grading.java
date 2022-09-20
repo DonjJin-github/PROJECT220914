d676fc4772ed8323b691d39b8543db605dc4e11a
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2022-09-20 20:03:01 +0900

    Update Grading.java

--- Grading.java
+++ Grading.java
@@ -5,17 +5,17 @@ public class Grading {
         char grade;
         Scanner scanner = new Scanner(System.in);
         System.out.print("점수를 입력하세요(0~100): ");
-        int score = scanner.nextInt(); // 점수 읽기
+        int score = scanner.nextInt(); 
 
-        if(score >= 90) // score가 90 이상
+        if(score >= 90) 
             grade = 'A';
-        else if(score >= 80) // score가 80 이상 90 미만
+        else if(score >= 80) 
             grade = 'B';
-        else if(score >= 70) // score가 70 이상 80 미만
+        else if(score >= 70)
             grade = 'C';
-        else if(score >= 60) // score가 60 이상 70 미만
+        else if(score >= 60) 
             grade = 'D';
-        else // score가 60 이만
+        else 
             grade = 'F';
         System.out.println("학점은 "+ grade + "입니다.");
 
