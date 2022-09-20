9d78d607b3e53ce5b0fb1b4f9910084ba20991ce
DonjJin-github <101619115+DonjJin-github@users.noreply.github.com>
2022-09-20 20:04:01 +0900

    Update NestedIf.java

--- NestedIf.java
+++ NestedIf.java
@@ -11,15 +11,15 @@ public class NestedIf {
         System.out.print("학년을 입력하세요(1~4): ");
         int year = scanner.nextInt();
 
-        if(score >= 60) { // 60점 이상
+        if(score >= 60) { 
             if(year != 4)
-                System.out.println("합격!");       // 4학년 아니면 합격
+                System.out.println("합격!");       
             else if(score >= 70)
-                System.out.println("합격!");      // 4학년이 70점 이상이면 합격
+                System.out.println("합격!");    
             else
-                System.out.println("불합격!");     // 4학년이 70점 미만이면 불합격
+                System.out.println("불합격!");   
         }
-        else // 60점 미만 불합격
+        else 
             System.out.println("불합격!");
 
         scanner.close();
